dc213d24b91f307db65f5c836a9e14c20bc07ab3
Robbie  H <ecofemale@hotmail.com>
2019-02-19 00:31:32 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(board, token)
   board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-  token = ["X", "O"]
+  index = ["X", "O"]
   
 end   
